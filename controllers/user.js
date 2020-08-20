2225e849641a8dd119a4140f69a696c42d04998b
Ralphie <ralphie94@pacbell.net>
2020-08-20 02:07:35 -0700

    implemented jwt for user signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
@@ -15,6 +17,27 @@ exports.signup = (req, res) => {
         res.json({
             user
         });
+    });
+};
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({ email }, (err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: "User with that email does not exist. Please register."
+            });
+        }
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+        res.cookie("t", token, { expire: new Date() + 9999 })
+        const { _id, name, email, role } = user
+        return res.json({ token, user: { _id, email, name, role }});
     });
 };
 
