b78103fd799ccb9b30988ee20c48b89c4b4168b5
Ralphie <ralphie94@pacbell.net>
2020-08-20 02:25:30 -0700

    changed user to auth

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 require("dotenv").config();
-const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 
-app.use("/api", userRoutes);
+app.use("/api", authRoutes);
 
 const port = process.env.PORT || 8000
 
