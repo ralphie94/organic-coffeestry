f21f942bbcffeddda0f3e1f9bbd1e8a42f989179
Ralphie <ralphie94@pacbell.net>
2020-08-20 22:22:49 -0700

    created method to list most popular and newest products

--- models/product.js
+++ models/product.js
@@ -21,6 +21,10 @@ const productSchema = new mongoose.Schema({
         quantity: {
             type: Number,
         },
+        sold: {
+            type: Number,
+            default: 0
+        },
         photo: {
             data: Buffer,
             contentType: String
