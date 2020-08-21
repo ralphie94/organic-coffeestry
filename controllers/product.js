71d71ca70cb4b3b2ae9a45151db286a4e0f44f2b
Ralphie <ralphie94@pacbell.net>
2020-08-20 22:46:43 -0700

    created method to fetch photo of product

--- controllers/product.js
+++ controllers/product.js
@@ -132,3 +132,11 @@ exports.list = (req, res) => {
             res.send(products);
         });
 };
+
+exports.photo = (req, res, next) => {
+    if(req.product.photo.data) {
+        res.set("Content-Type", req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
