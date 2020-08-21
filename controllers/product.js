ab914c6807056803790a0d09e9f5dcb03c8b9130
Ralphie <ralphie94@pacbell.net>
2020-08-20 20:36:11 -0700

    implemented validators for image size and required fields

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,22 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+        const {name, description, price, quantity, shipping} = fields
+
+        if(!name || !description || !price || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
         let product = new Product(fields)
 
         if(files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
