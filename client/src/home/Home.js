b18f21e587b0e15a92ef55ab0ce4da0beac86d8e
Ralphie <ralphie94@pacbell.net>
2020-08-21 19:26:48 -0700

    built out and styled signup component

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Home = () => <div>Home</div>;
+const Home = () => <div>Home {process.env.REACT_APP_API_URL}</div>;
 
 export default Home;
--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { API } from "../config";
 
-const Signup = () => <div>Signup</div>;
+import "./Signup.css"
+
+const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value })
+    };
+
+    const signUpForm = () => (
+        <form>
+            <div className="login-container">
+                <h1 className="register-title">Register</h1>
+                <h2 className="username">Username</h2>
+                <input onChange={handleChange("name")} type="text" className="name-register" name="username"></input><br/>
+                <h2 className="password">Password</h2>
+                <input onChange={handleChange("email")} type="email" className="email-box" name="password"></input><br/>
+                <h2 className="email">Email</h2>
+                <input onChange={handleChange("password")} type="password" className="password-register" name="password"></input><br/>
+                <button type="submit" className="register-btn" value="Submit">CREATE</button>
+            </div>
+        </form>
+    )
+
+    return (
+        <div>
+            {signUpForm()}
+        </div>
+    );
+};
 
 export default Signup;
