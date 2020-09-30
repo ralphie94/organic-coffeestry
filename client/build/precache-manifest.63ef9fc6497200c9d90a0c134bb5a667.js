1a59650a9f80c15dfb52f9cc235c0f5f56f8327e
ralphieadmin <ralphie94@pacbell.net>
2020-09-30 01:04:26 +0000

    fixing

---
+++ client/build/precache-manifest.63ef9fc6497200c9d90a0c134bb5a667.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee94000d76acba9b26ca7c41f4824db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df33b57fa30be04001fa",
+    "url": "/static/css/main.03e250ba.chunk.css"
+  },
+  {
+    "revision": "777a97a97c8c9b067ae1",
+    "url": "/static/js/2.949ee1e1.chunk.js"
+  },
+  {
+    "revision": "3ee38a7412dc9aa51081970296d8baba",
+    "url": "/static/js/2.949ee1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df33b57fa30be04001fa",
+    "url": "/static/js/main.786ee3a5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "81ae3f75f95559970b3f9d3cfd20eadc",
+    "url": "/static/media/coffee-cup.81ae3f75.jpg"
+  },
+  {
+    "revision": "8f496d0c41125c900f65ce9464d4d8f1",
+    "url": "/static/media/coffee-pour.8f496d0c.jpeg"
+  },
+  {
+    "revision": "a7f0d58d1de63336092c8249381d3aed",
+    "url": "/static/media/organic-coffeestry.a7f0d58d.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c209ae2a0307c84915cfd9eb1e822f48.js"
+  "/precache-manifest.63ef9fc6497200c9d90a0c134bb5a667.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
