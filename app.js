9339d51356cc03fed51f62161458eab710547b77
Ralphie <ralphie94@pacbell.net>
2022-01-07 18:13:21 -0800

    final deployment prep

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 } else {
     app.get("/", (req, res) => {
