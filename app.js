bc5693d38486e31c5b5c078b669af276bddeee26
Ralphie <ralphie94@pacbell.net>
2022-01-07 18:10:40 -0800

    final deployment prep

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if(process.env.NODE_ENV === "production") {
     });
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
